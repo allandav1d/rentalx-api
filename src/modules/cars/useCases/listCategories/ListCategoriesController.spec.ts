86a38868d693d3d898604a4f9e1dad24ae9ad6d1
AllanDav1d <allan.j.k15@gmail.com>
2022-03-18 20:43:34 -0300

    Teste de integração

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,54 @@
+import request from 'supertest'
+import { hash } from 'bcrypt';
+import { v4 as uuidV4 } from 'uuid';
+import { Connection } from 'typeorm';
+
+import { app } from '@shared/infra/http/app';
+
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+describe("List Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin', 8);
+
+    await connection.query(`INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license) 
+    values('${id}', 'Admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXXXX')`
+    );
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
+  it('should be able to list all categories', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin'
+    })
+
+    const { token } = responseToken.body;
+
+    await request(app).post('/categories')
+      .send({
+        name: "Supertest",
+        description: "Categoria de carro Supertest"
+      })
+      .set({
+        Authorization: `Bearer ${token}`
+      });
+
+    const response = await request(app).get('/categories')
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  })
+
+
+})
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,8 +5,9 @@ export default async (host = 'database', port = 5432): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
-      port
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      port: process.env.NODE_ENV === 'test' ? 5433 : 5432,
+      database: process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database,
     }),
   );
 }
