887b34a571ae6fc51bb00e9ef198804ed4dad9e3
AllanDav1d <allan.j.k15@gmail.com>
2022-03-26 22:47:15 -0300

    fix: database conection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database', port = 5432): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
-      port: process.env.NODE_ENV === 'test' ? 5433 : 5432,
       database: process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database,
     }),
   );
