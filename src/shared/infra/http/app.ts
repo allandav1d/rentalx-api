e6fd6418c99dd09a09771386af680dc04f53a9b5
AllanDav1d <allan.j.k15@gmail.com>
2022-03-27 01:01:07 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import cors from "cors";
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors"
 import swaggerUi from 'swagger-ui-express';
@@ -15,11 +16,13 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
