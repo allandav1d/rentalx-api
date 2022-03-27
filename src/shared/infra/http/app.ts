6793fabc955a2638ecc179285a170f03dd251f70
AllanDav1d <allan.j.k15@gmail.com>
2022-03-27 02:22:56 -0300

    radis

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,18 +11,21 @@ import "@shared/container";
 import { router } from '@shared/infra/http/routes';
 import swaggerFile from "../../../swagger.json";
 import { AppError } from '@shared/errors/AppError';
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 
+//app.use(rateLimiter);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
-app.use(cors());
+//app.use(cors());
 app.use(router);
 
 app.use(
---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import * as redis from "redis";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import { AppError } from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  legacyMode: true,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 5,
+  duration: 5,
+})
+
+export default async function rateLimiter(request: Request, response: Response, next: NextFunction): Promise<void> {
+  try {
+
+    await limiter.consume(request.ip);
+
+    next();
+  } catch (err) {
+    throw new AppError("Too many requests", 429);
+  }
+}
